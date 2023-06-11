a956a2c9392637bebb864fdd1e0d5ade52706baf
cjw <cjwcommuny@outlook.com>
2023-06-11 12:39:35 +0800

    test: add test for iter.Zip

--- iter/iter_test.go
+++ iter/iter_test.go
@@ -1,6 +1,8 @@
 package iter
 
 import (
+	"github.com/barweiss/go-tuple"
+	"math"
 	"reflect"
 	"testing"
 	"testing/quick"
@@ -52,6 +54,28 @@ func TestCount(t *testing.T) {
 	f := func(s []int) bool {
 		count := fn.Pipe2(SliceIter[int], Count[int])(s)
 		return count == len(s)
+	}
+	quickTest(t, f)
+}
+
+func TestZip(t *testing.T) {
+	f := func(s1 []int, s2 []int) bool {
+		zipped := fn.Pipe3(
+			SliceIter[int],
+			Zip[int, int](SliceIter(s1)),
+			CollectToSlice[tuple.T2[int, int]],
+		)(s2)
+		expectedLength := int(math.Min(float64(len(s1)), float64(len(s2))))
+		sameLength := len(zipped) == expectedLength
+		sameElements := func() bool {
+			for index := 0; index < expectedLength; index++ {
+				if s1[index] != zipped[index].V1 || s2[index] != zipped[index].V2 {
+					return false
+				}
+			}
+			return true
+		}()
+		return sameLength && sameElements
 	}
 	quickTest(t, f)
 }
